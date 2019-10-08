922813444894dc562233098da04a02bad64a830c
Aricati <mirza.arnautovic@outlook.com>
2019-10-08 12:15:34 +0200

    Updates

---
+++ precache-manifest.3f037d0a17ffa02c46e735a6697d1fe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e2eb95c26c93e45221f1c0e8a879e68",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "d1487163dbe4ec6bb728",
+    "url": "/ContactManager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b973ea0fbe0bf5a53012",
+    "url": "/ContactManager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d1487163dbe4ec6bb728",
+    "url": "/ContactManager/static/js/2.703cec77.chunk.js"
+  },
+  {
+    "revision": "b973ea0fbe0bf5a53012",
+    "url": "/ContactManager/static/js/main.db7de87c.chunk.js"
+  },
+  {
+    "revision": "112eb2a9bc0338f97a28",
+    "url": "/ContactManager/static/js/runtime~main.0e09fb07.js"
+  }
+]);
